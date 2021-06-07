adc179843d850fe90511220f88dbe6b0dd1e3990
lee <aujblee@foxmail.com>
2021-06-07 11:16:52 +0800

    update precess.py

--- precess.py
+++ precess.py
@@ -6,9 +6,16 @@ img1 = cv2.imread('cap1.jpg')
 img1 = cv2.GaussianBlur(img1, (5, 5), 0)
 img2 = cv2.imread('cap2.jpg')
 img2 = cv2.GaussianBlur(img2, (5, 5), 0)
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 diff = img1 - img2
+# diff = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
 
+plt.imshow(diff, cmap='gray')
+plt.show()
 
-diff = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
-plt.imshow(diff)
+diff[diff < 150] = 0
+# diff = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
+plt.imshow(diff, cmap='gray')
 plt.show()
