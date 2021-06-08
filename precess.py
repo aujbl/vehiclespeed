556ea1a9cec5fb6a8a9769837351fb7410d30559
lee <aujblee@foxmail.com>
2021-06-08 11:11:44 +0800

    update 11:11

--- precess.py
+++ precess.py
@@ -1,17 +1,46 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure()
-img1 = cv2.imread('cap1.jpg')
-img2 = cv2.imread('cap2.jpg')
+plt.figure(0, figsize=(16, 8))
 
-raw = img1
-gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-gray1 = cv2.GaussianBlur(gray1, (7, 7), 0)
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.GaussianBlur(gray2, (7, 7), 0)
+def preprocess(frame, k_size=5, fileter='Gauss', threshold=60, op=None):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (k_size, k_size), 0.) if fileter == 'Gauss' \
+            else cv2.medianBlur(gray, k_size)
+    _, thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+    if op:
+        op = cv2.getStructuringElement(op, (k_size, k_size))
+        thresh = cv2.erode(thresh, op)
+        thresh = cv2.dilate(thresh, op)
+    return gray, thresh
 
-gray = cv2.absdiff(gray1, gray2)
+def frame_diff(frames):
+    diff_1 = cv2.absdiff(frames[1], frames[0])
+    diff_2 = cv2.absdiff(frames[2], frames[1])
+    diff = cv2.absdiff(diff_2, diff_1)
+    return diff
 
-plt.imshow(gray, cmap='gray')
-plt.show()
+cap = cv2.VideoCapture('video2.mp4')
+ret, frame = cap.read()
+_, frame = preprocess(frame, k_size=5, fileter='m', op=cv2.MORPH_ELLIPSE)
+frames = [frame] * 3
+while ret:
+    ret, frame = cap.read()
+    _, frame = preprocess(frame, k_size=5, fileter='m', op=cv2.MORPH_ELLIPSE)
+    frames.pop(0)
+    frames.append(frame)
+    diff = frame_diff(frames)
+
+    contours, _ = cv2.findContours(diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        box = cv2.boundingRect(contour)
+        x, y, w, h = box
+        diff = cv2.rectangle(diff, (x, y), (w, h), (0, 255, 0))
+
+    plt.imshow(diff, cmap='gray')
+    plt.pause(0.0001)
+    plt.clf()
+cap.release()
+
+# plt.show()
